7e122bf0bf2fd003cc2b79c2d7e13591fc08ca0a
mehedih23 <nmehedi23@gmail.com>
2022-05-22 00:13:56 +0600

    banner done finally

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import './App.css';
 import Home from './Pages/Home/Home';
 import Footer from './Pages/Shared/Footer';
 import Navbar from './Pages/Shared/Navbar';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    AOS.init();
+  }, [])
   return (
     <div className='app'>
       <Navbar></Navbar>
--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -3,13 +3,40 @@ import React from 'react'
 const Banner = () => {
     return (
         <div class="hero min-h-screen" style={{ "backgroundImage": "url(https://html.modernwebtemplates.com/electrix/images/slide01.jpg)" }}>
-            <div class="hero-overlay bg-opacity-60"></div>
+            <div class="hero-overlay bg-opacity-50"></div>
             <div class="hero-content text-center text-neutral-content">
                 <div class="max-w-md">
-                    <p className='text-2xl text-center'>ELECTRIX MANUFACTURING</p>
-                    <h1 class="mb-5 text-5xl"><span className='text-secondary font-bold'>COMMITED</span> <br /> TO SUPER QUALITY</h1>
-                    <p class="mb-5">We won Many Industrial Awards and Got Many Certificates Since 2001-2016</p>
-                    <button class="btn btn-primary">Get Started</button>
+                    <p
+                        data-aos="fade-down"
+                        data-aos-duration="1000"
+                        data-aos-delay="200"
+                        className='text-2xl text-center'
+                    >ELECTRIX MANUFACTURING</p>
+                    <div
+                        data-aos="fade-down"
+                        data-aos-duration="800"
+                        data-aos-delay="600"
+                        class="mb-5 text-5xl"
+                    >
+                        <span className='text-secondary font-bold'>COMMITED</span>
+                        <br />
+                        <span>TO SUPER QUALITY</span>
+                    </div>
+                    <p
+                        data-aos="fade-down"
+                        data-aos-duration="600"
+                        data-aos-delay="1000"
+                        class="mb-5"
+                    >We won Many Industrial Awards and Got Many Certificates Since 2001-2016</p>
+                    <div
+                        data-aos="fade-down"
+                        data-aos-duration="400"
+                        data-aos-delay="1200"
+                    >
+                        <button
+                            class="btn btn-primary"
+                        >Get Started</button>
+                    </div>
                 </div>
             </div>
         </div>
