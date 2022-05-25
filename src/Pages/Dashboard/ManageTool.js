c35122d59da3dee5cdbd242a58031beb46d10396
mehedih23 <nmehedi23@gmail.com>
2022-05-25 11:30:44 +0600

    manage tools done

---
+++ src/Pages/Dashboard/ManageTool.js
@@ -0,0 +1,73 @@
+import { signOut } from 'firebase/auth'
+import React from 'react'
+import toast from 'react-hot-toast'
+import { useNavigate } from 'react-router-dom'
+import Swal from 'sweetalert2'
+import auth from '../../firebase.init'
+
+const ManageTool = ({ tool, index, refetch }) => {
+    const navigate = useNavigate()
+    const handleDelete = () => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                Swal.fire(
+                    'Deleted!',
+                    'Your item has been deleted.',
+                    'success',
+                    fetch(`http://localhost:1111/tool/${tool._id}`, {
+                        method: 'DELETE',
+                        headers: {
+                            'authorization': `Bearer ${localStorage.getItem('Access-Token')}`
+                        }
+                    })
+                        .then(response => {
+                            if (response.status === 401 || response.status === 403) {
+                                signOut(auth);
+                                toast.error('Token expired', { id: 'token-error' });
+                                navigate('/');
+                            } else {
+                                return response.json()
+                            }
+                        })
+                        .then(data => {
+                            if (data.acknowledged) {
+                                refetch();
+                            }
+                        })
+
+                )
+            }
+        })
+
+    }
+    return (
+        <tr>
+            <th>{index + 1}</th>
+            <td>
+                <div class="avatar">
+                    <div class="w-16 rounded">
+                        <img src={tool.image} alt="Tailwind-CSS-Avatar-component" />
+                    </div>
+                </div>
+            </td>
+            <td>{tool.name}</td>
+            <td>
+                <button
+                    onClick={handleDelete}
+                    className='btn btn-error btn-sm'
+                >Delete</button>
+            </td>
+        </tr>
+    )
+}
+
+export default ManageTool
--- src/Pages/Dashboard/ManageTools.js
+++ src/Pages/Dashboard/ManageTools.js
@@ -1,8 +1,44 @@
 import React from 'react'
+import { useQuery } from 'react-query'
+import ManageTool from './ManageTool'
 
 const ManageTools = () => {
+    const { isLoading, error, data: tools, refetch } = useQuery('repoData', () =>
+        fetch('http://localhost:1111/tools').then(res =>
+            res.json()
+        )
+    )
+
+    if (isLoading) return 'Loading...'
+
+    if (error) return 'An error has occurred: ' + error.message
+
     return (
-        <div>ManageTools</div>
+        <div className='px-1 md:px-2 lg:px-4'>
+            <h3 className='text-3xl my-4'>Manage all tools</h3>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    <thead>
+                        <tr>
+                            <th>sl no.</th>
+                            <th>Item</th>
+                            <th>Name</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            tools.map((tool, index) => <ManageTool
+                                key={tool._id}
+                                tool={tool}
+                                index={index}
+                                refetch={refetch}
+                            ></ManageTool>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
     )
 }
 
